73df942693e70544e18989be87c342d829cadde3
codeKK <kunalkenge12345@gmail.com>
2020-12-24 14:44:40 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DataLayer } from "./DataLayer";
+import { initialState } from "./reducer";
+import reducer from "./reducer";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
